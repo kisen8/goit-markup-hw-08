74d139296614f9f1ff04e4b1a76d8aec6c5f5c5d
Ihor Komar <kisenbenz8@gmail.com>
2022-05-25 03:11:35 +0300

    hw-8 header + menu.js

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.mob-menu-open-btn'),
+    closeMenuBtn: document.querySelector('.mob-menu-close-btn'),
+    menu: document.querySelector('.mobile-menu'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-open');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
